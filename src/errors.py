b73a4b8e1bf46cdafedf5d090f6a211740f40dca
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2025-07-30 01:30:50 +0100

    use isinstance instead of match

--- src/errors.py
+++ src/errors.py
@@ -7,21 +7,15 @@ class StdinIsTTYError(Exception):
 
 
 def is_error_unexpected(exc: Exception) -> bool:
-    match exc:
-        case UserDataDirError():
-            return True
-        case _:
-            return False
+    return isinstance(exc, UserDataDirError)
 
 
 def error_follow_up(exc: Exception) -> str | None:
-    match exc:
-        case StdinIsTTYError():
-            return """\
+    if isinstance(exc, StdinIsTTYError):
+        return """\
 Tip: Pass query via stdin as follows:
   echo 'MATCH (n) RETURN n LIMIT 5' | graphc -q -
   graphc -q - < query.cypher
   graphc -q - <<< 'MATCH (n) RETURN n LIMIT 5'\
 """
-        case _:
-            return None
+    return None
